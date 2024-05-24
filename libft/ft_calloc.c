acc2d53aca238087842d48f75ef9c9eaa36126eb
Burak Celiloglu <bkceliloglu@gmail.com>
2024-05-24 16:32:27 +0300

    calloc function added.

---
+++ libft/ft_calloc.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+    void    *ptr;
+
+    if (count == 0 || size == 0)
+    {
+        count = 1;
+        size = 1;
+    }
+    ptr = (void *)malloc(count * size);
+    if (ptr)
+        ft_bzero(ptr, (count * size));
+    return (ptr);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -7,7 +7,8 @@ typedef struct	s_list
 	struct s_list	*next;
 }	t_list;
 
-#include <stdio.h>
+# include <stdio.h>
+# include <stdlib.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -30,5 +31,6 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
+void	*ft_calloc(size_t count, size_t size);
 
 #endif
