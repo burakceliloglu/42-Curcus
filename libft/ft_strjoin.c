8e6f40d04338064778d9fe16ef1924f80d415c8f
Burak Celiloglu <bkceliloglu@gmail.com>
2024-05-25 23:50:01 +0300

    strjoin and substr modified.

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -3,31 +3,31 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*join;
-	int	i;
-	int	j;
+	int		i;
+	int		j;
 
 	join = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	i = 0;
 	if (!join)
 		return (NULL);
-	while (s1[i] = '\0')
+	while (i < ft_strlen(s1))
 	{
 		join[i] = s1[i];
 		i++;
 	}
-	j = 0;
-	while (s2[j] = '\0')
+	while (i < ft_strlen(s1) + ft_strlen(s2))
 	{
-		join[i] = s2[j];
+		join[i] = s2[i - ft_strlen(s1)];
 		i++;
-		j++;
 	}
 	join[i] = '\0';
 	return (join);
 }
 
-#include <stdio.h>
 int	main()
 {
 	printf("%s", ft_strjoin("Burak","Celiloğlu"));
 }
+
+// burada while(s1[i] != '\0') kullanamıyoruz.
+// çünkü const char tanımlı.
--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -3,10 +3,10 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*subs;
-	int	i;	
+	int		i;
 
 	subs = (char *)malloc(sizeof(char) * (len + 1));
-	if (subs)
+	if (!subs)
 		return (NULL);	
 	i = 0;
 	while (start <= len)
