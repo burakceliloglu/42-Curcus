6e3d7e384d6ff4832b916ae3dff68de8fe04a646
Burak Celiloglu <bkceliloglu@gmail.com>
2024-06-06 15:58:05 +0300

    fd functions added.

--- libft/libft.h
+++ libft/libft.h
@@ -44,6 +44,8 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 //void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
 #endif
